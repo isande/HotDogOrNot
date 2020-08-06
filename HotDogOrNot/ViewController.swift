1d2d9fe30686acf84475a38fd6cd4a9e59dda3a8
Peggy Wollenhaupt <isande@gmail.com>
2020-08-06 08:57:19 -0600

    add camera functionality

--- HotDogOrNot/ViewController.swift
+++ HotDogOrNot/ViewController.swift
@@ -14,12 +14,35 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     @IBOutlet weak var imageView: UIImageView!
     
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            
+            imageView.image = selectedImage
+            
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
+
         
     }
 
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        present(imagePicker, animated: true, completion: nil)
+        
     }
     
 }
