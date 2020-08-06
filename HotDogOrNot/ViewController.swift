6bef95a4d0446c95731a148742defe5b1eff82ab
Peggy Wollenhaupt <isande@gmail.com>
2020-08-06 08:14:44 -0600

    initial commit

---
+++ HotDogOrNot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HotDogOrNot
+//
+//  Created by Peggy Wollenhaupt on 8/6/20.
+//  Copyright © 2020 Peggy Wollenhaupt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
